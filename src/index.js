e48eca93a5aff45358f5a3d973d4f806173269af
Cyrobo <25550736+Cyrobo@users.noreply.github.com>
2018-01-24 18:25:50 +0000

    Cleaned Up Index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const path = require('path');
 const discord = require('discord.js');
 const bot = new discord.Client();
-const globalAdmins = ['123778278086541313', '288209033247326208', '353530628370726913'];//CyRob, DarkHermor & Ato
-const environmentConfigPath = [__dirname, 'config', `${process.env.ENVIRONMENT}.json`].join(path.sep);
+const globalAdmins = ['123778278086541313', '288209033247326208', '353530628370726913'];
+// User ID's of CyRob, DarkHermor & Ato
 const dmChannelId = '398940588063457292';
+// Channel to send Dm's to, Currently: #enfer-log
 
 const reload = require('./commands/reload');
 
@@ -21,22 +21,23 @@ reload.run(bot);
 
 bot.on('ready', () => {
   console.warn(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
-  //bot.user.setGame(`on ${bot.guilds.size} servers`);
-  
-  bot.user.setGame(`Hacking the Argosy`);//Until I sort out storing data between sessions
+  // Disabled for now //bot.user.setGame(`on ${bot.guilds.size} servers`);
+  bot.user.setGame(`Learning to play the piano`);
   bot.user.setStatus(`dnd`);
+  // Until I sort out storing data between sessions
 });
 
 bot.on('message', async message => {
-  console.log(`[${message.createdTimestamp}]: ${message.channel.id}(${message.channel.type}) [${message.author.tag}]: ${message.content}`)
+  console.log(`[${message.createdTimestamp}]: ${message.channel.id}(${message.channel.type}) [${message.author.tag}]: ${message.content}`);
   if (message.author.bot) return;
-  try{
-    if (message.channel.type === 'dm') {bot.channels.get(dmChannelId).send(`DM from ${message.author.tag}: ${message.content}`);}//print dm's to channel (so they can be seen)
+  try {
+    if (message.channel.type === 'dm') {
+      bot.channels.get(dmChannelId).send(`DM from ${message.author.tag}: ${message.content}`);
+    }
+    // Print dm's to channel (so they can be seen)
+  } catch (e) {
+    console.warn(`Error Posting Dm: ${e}`);
   }
-  catch(e)
-    {
-      console.log(`Error Posting Dm: ${e}`);
-    }	
   if (message.content.indexOf(bot.config.prefix) !== 0) return;
 
   const args = message.content.split(/ +/g);
@@ -50,9 +51,14 @@ bot.on('message', async message => {
     if (!role || !message.member.roles.has(role.id)) return;
   }
 
-  bot.commands[command].run(bot, message, args);
+  bot.commands[command].run(bot, message, args, globalAdmins);
 });
 
 bot.login(bot.config.token);
 
-process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));//stops bot crashing
+process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));
+// Stops bot crashing
+process.on('ECONNRESET', error => {
+  console.error(`Error: ${error}`);
+});
+// Attempt at handling connection resets but fails as it's a thrown error
